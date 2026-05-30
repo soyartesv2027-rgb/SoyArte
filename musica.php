7a143e450319f391b71f16d1cb4e5235caf4da8a
xjeffersonx293 <jefferson.garcia2027@adoc.superate.org.sv>
2026-05-30 00:45:50 -0600

    perfil,musica y tienda terminada

--- musica.php
+++ musica.php
@@ -10,6 +10,7 @@ session_start();
     <title>SoyArte-Musica</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <link rel="stylesheet" href="styles/musica.css">
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
@@ -78,7 +79,19 @@ session_start();
     </button>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <script src="JavaScrip/script.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+  <script>
+    window.addEventListener("scroll", () => {
+      const section = document.querySelector(".info-soyarte");
+      if (section) {
+        const position = section.getBoundingClientRect().top;
+        const screen = window.innerHeight;
+        if (position < screen - 100) {
+          section.classList.add("visible");
+        }
+      }
+    });
+  </script>
+<script src="JavaScrip/script.js"></script>
 </body>
 </html>
