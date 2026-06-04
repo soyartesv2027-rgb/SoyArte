970ec4494302fca6c4fe43686451d83748a5e4e8
xjeffersonx293 <jefferson.garcia2027@adoc.superate.org.sv>
2026-06-03 22:55:59 -0600

    actualizacion

--- poesia.php
+++ poesia.php
@@ -25,6 +25,7 @@ $resultado = $stmt->get_result();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="styles/poesia.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
